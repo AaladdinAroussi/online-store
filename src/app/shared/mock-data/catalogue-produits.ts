189403ac6415df8ad3e0d314999156959a5e31ae
aladdin laaroussi <123190776+AaladdinAroussi@users.noreply.github.com>
2023-12-21 15:46:10 +0100

    Add files via upload

---
+++ src/app/shared/mock-data/catalogue-produits.ts
@@ -0,0 +1,12 @@
+import { Produit } from "../models/produit";
+export const CATALOGUE : Produit [] = [
+    {nom: 'Apple MacBook Pro 14 M3 Max', prix :'1099dt', categId : 'C1',image :'../../assets/img4.jpg', description :'CPU 8 cœurs avec 4 cœurs de performance et 4 cœurs à haute efficacité énergétique' },
+    {nom: 'Apple MacBook Pro 16 M2 Pro', prix :'9999dt', categId : 'C2',image : '../../assets/img5.jpg',description :'portable polyvalent de 16,2 pouces annoncé en janvier 2023. Il est équipé de trois ports Thunderbolt 4'},
+    {nom: 'Apple MacBook Pro 14 2021', prix :'8999dt', categId : 'C3',image : '../../assets/img6.jpg',description :'Performances de haut vol,Excellent écran mini-led,Autonomie énorme'},
+    {nom: 'Samsung Galaxy Book3 Ultra', prix :'7559dt', categId : 'C4',image :'../../assets/img7.jpg', description :'16.00-inch display with a resolution of 1800x2880 pixels1' },
+    {nom: 'Galaxy Book2 Pro 15 ', prix :'6997dt', categId : 'C5',image : '../../assets/img8.jpg',description :'16.00-inch display with a resolution of 1800x2880 pixels1'},
+    {nom: 'Asus ZenBook 15 Oled', prix :'5999dt', categId : 'C6',image : '../../assets/img9.jpg',description :'Ecran 15,6 pouces OLED 2.8K (2880 x 1620) - 0.2 ms - 120Hz'},
+    {nom: 'MacBook Air M1', prix :'53999dt', categId : 'C7',image :'../../assets/img10.jpg', description :'Performances de haut vol,Excellent écran mini-led,Autonomie énorme' },
+    {nom: ' Honor MagicBook 15', prix :'4999dt', categId : 'C8',image : '../../assets/img11.jpg',description :'Performances de haut vol,Excellent écran mini-led,Autonomie énorme'},
+    {nom: 'Rog Strix G15', prix :'4550dt', categId : 'C9',image : '../../assets/img12.jpg',description :'Performances de haut vol,Excellent écran mini-led,Autonomie énorme'},
+    ];
